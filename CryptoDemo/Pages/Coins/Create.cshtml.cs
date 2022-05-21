7f05c93b790d0f695dc0cc26d447dcedb8c7f279
ramaz <ramaz@DESKTOP-UQR20Q0>
2022-05-21 16:55:36 +0300

    Add project files.

---
+++ CryptoDemo/Pages/Coins/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CryptoDemo.Data;
+using CryptoDemo.Models;
+
+namespace CryptoDemo.Pages.Coins
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CryptoDemo.Data.CryptoDemoContext _context;
+
+        public CreateModel(CryptoDemo.Data.CryptoDemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Coin Coin { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Coin == null || Coin == null)
+            {
+                return Page();
+            }
+
+            _context.Coin.Add(Coin);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CryptoDemo/Pages/Coins/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CryptoDemo.Data;
+using CryptoDemo.Models;
+
+namespace CryptoDemo.Pages.Coins
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CryptoDemo.Data.CryptoDemoContext _context;
+
+        public DetailsModel(CryptoDemo.Data.CryptoDemoContext context)
+        {
+            _context = context;
+        }
+
+      public Coin Coin { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Coin == null)
+            {
+                return NotFound();
+            }
+
+            var coin = await _context.Coin.FirstOrDefaultAsync(m => m.Id == id);
+            if (coin == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Coin = coin;
+            }
+            return Page();
+        }
+    }
+}
